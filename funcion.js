fd40e2f10c992936c254dcdfd09bc96a54bd8d7b
Ruth <gracielaruthg@gmail.com>
2020-06-14 19:58:31 -0300

    Codigo de funcion terminado

--- funcion.js
+++ funcion.js
@@ -1,34 +1,35 @@
 
-function  recorrer() {
+function  recorrer(m) {
+  
    let  arrays= [];  
     
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < m; i++) {
+        
         arrays.push(i)   
+
       
     }
     return arrays
    
 }
-let s = recorrer()  
- 
-
- 
 
 function  sumar(adicion) {
  let sum = (a , b)=> a + b
 return  adicion.reduce(sum); 
     
 } 
+let s = recorrer(10)
+console.log(sumar(s));
 
 
-
-
-
-function name() {
-    let primero = [20,30,40]
-    let segundo = sumar([37,90])
-    console.log(segundo);
-    console.log(sumar(primero));
-    console.log(sumar(s));
-}
-name()
+// function name() {
+    
+//     let s = recorrer()  
+//     let primero = [20,30,40]
+//     let segundo = sumar([37,90])
+   
+//     console.log(segundo);
+//     console.log(sumar(primero));
+//     console.log(sumar(s));
+// }
+// name()
